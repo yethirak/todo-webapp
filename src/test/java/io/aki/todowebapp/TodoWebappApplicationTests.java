e06b853df5b771d50d3d7e8d7ebaac91893f2762
Akhilesh <akhil.it.2001@gmail.com>
2022-12-26 02:31:38 +0800

    initial commit

---
+++ src/test/java/io/aki/todowebapp/TodoWebappApplicationTests.java
@@ -0,0 +1,13 @@
+package io.aki.todowebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoWebappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
